6bde3be1b58eb7b95a4628a7ef65f6ae2b267c9f
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-09-25 14:47:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assgn01_Calculator
+{
+    public class Numbers
+    {
+        public int Number { get; set; }
+    }
+    internal class Program
+    {
+        static void Calculate(string input)
+        {
+            //Numbers num = new Numbers();
+            foreach(char c in input)
+            {
+                int a;
+                if (char.IsDigit(c))
+                {
+                    a = int.Parse(Convert.ToString(c)); //take each member of the string array
+
+                    Console.WriteLine(a);
+                }
+                else if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    Console.WriteLine("Character");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator: ");
+            Console.Write("Input: ");
+            string input = (Console.ReadLine());
+
+            Calculate(input);
+           
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assgn01_Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assgn01_Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec9ad9fd-1374-49f6-b133-eb87825026c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
