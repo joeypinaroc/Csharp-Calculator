28b73c80af5d569b822a0403f444a24912dec377
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-09-30 12:43:50 -0600

    Changed Lists to Stacks

--- Program.cs
+++ Program.cs
@@ -10,22 +10,25 @@ using System.Threading.Tasks;
  * The logic for traversing the lists and evaluating a math expression 
  * is taken from the following reference:
  *      1. https://www.geeksforgeeks.org/expression-evaluation/
+ * Reference 1 used Stacks instead of Lists, to conveniently pop() the elements 
+ * on top of a char list while traversing the Stack and performing the math operations.
+ * 
  */
 namespace Assgn01_Calculator
 {
     internal class Program
     {
-        static void Calculate(string input)
+        static double Calculate(string input)
         {
-            List<int> numbers = new List<int>(); // List to store numbers from input expression
-            List<char> operators = new List<char>(); // List to store operators/parentheses from input exp            
+            Stack<double> numbers = new Stack<double>(); //Stack to store numbers from input expression
+            Stack<char> operators = new Stack<char>(); //Stack to store operators/parentheses from input exp            
 
-            char[] chars = input.ToCharArray(); // split input expression into a char array
+            char[] chars = input.ToCharArray(); //split input expression into a char array
             for(int i = 0; i < chars.Length; i++)
             {
-                if (chars[i] == ' ') continue; // if char is whitespace, skip
+                if (chars[i] == ' ') continue; //if char is whitespace, skip
 
-                if (char.IsDigit(chars[i])) // check if current char is a digit
+                if (char.IsDigit(chars[i])) //check if current char is a digit
                 {
                     /* Reference 1 used a StringBuilder to create multi-digit numbers */
                     StringBuilder sb = new StringBuilder();
@@ -33,27 +36,56 @@ namespace Assgn01_Calculator
                     {
                         sb.Append(chars[i++]);
                     }
-                    numbers.Add(int.Parse(sb.ToString())); //convert multi-digit string to int and add to numbers list
-                    i--; // since i is now on the next digit, decrease i to correct the offset
+                    numbers.Push(int.Parse(sb.ToString())); //convert multi-digit string to int and add to numbers list
+                    i--; //since i is now on the next digit, decrease i to correct the offset
                 }
 
                 else if (chars[i] == '+' || chars[i] == '-' || chars[i] == '*' || chars[i] == '/')
                 {
-                    operators.Add(chars[i]);
+                    while(operators.Count > 0)
+                    {
+                        numbers.Push(ApplyOperator(operators.Pop(), numbers.Pop(), numbers.Pop()));
+                    }
+                    operators.Push(chars[i]);
+                    //numbers.Push(ApplyOperator(operators.Pop(), numbers.Pop(), numbers.Pop())); //apply current operator to the last 2 numbers on top of Stack
                 }
             }
             PrintIntList(numbers);
             PrintCharList(operators);
+            while(operators.Count > 0)
+            {
+                numbers.Push(ApplyOperator(operators.Pop(),numbers.Pop(), numbers.Pop()));
+            }
+            return numbers.Pop();
 
         }
-        static void PrintIntList(List<int> list)
+        static double ApplyOperator(char op, double b, double a)
+        {
+            switch (op)
+            {
+                case '+': 
+                    return a + b;
+                case '-': 
+                    return a - b;
+                case '*': 
+                    return a * b;
+                case '/': 
+                    if(b == 0)
+                    {
+                        throw new Exception("Cannot be divided by zero");
+                    }
+                    return a / b;
+            }
+            return 0;
+        }
+        static void PrintIntList(Stack<double> list)
         {
             foreach(var item in list)
             {
                 Console.WriteLine(item);
             }
         }
-        static void PrintCharList(List<char> list)
+        static void PrintCharList(Stack<char> list)
         {
             foreach (var item in list)
             {
