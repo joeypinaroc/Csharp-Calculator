4dcf2912769fea53d7b212d2a3e913a07e30cfc6
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-10-03 20:31:32 -0600

    Updated documentation

--- Program.cs
+++ Program.cs
@@ -16,6 +16,21 @@ using System.Threading.Tasks;
  * Reference 2 below is from stackoverflow showing a ConvertToDouble method which I 
  * used to convert decimal numbers to double
  *      2. https://stackoverflow.com/questions/11399439/converting-string-to-double-in-c-sharp
+ *
+ * The plan is to:
+ *  - take input expression as string
+ *  - parse each char of the string and check if it is a number or an operator
+ *  - use 2 stacks to store numbers and operators
+ *  - once a number is encountered, push it to the numbers stack
+ *  - if an operator, or a parenthesis, is encountered, and the operators stack is empty, push that operator 
+ *    to the operators stack
+ *  - if another operator is encountered, check if it has more priority(precedence) than the last pushed operator
+ *      - if it has precedence, push it to operators stack
+ *      - if not, meaning the last pushed operator has precedence than the current, then proceed to perform that operation first
+ *  - once the entire expression has been parsed, proceed to performing the operations remaining
+ *  - performing the operation would involve the operator on top of the operators stack, and the last 2 numbers on top of the numbers stack
+ *      - pop the operators stack, and pop the numbers stack twice, and perform this operation
+ *  - each result from this would be pushed to the numbers stack, and so on, and return the last remaining number in the numbers stack
  */
 namespace Assgn01_Calculator
 {
@@ -67,23 +82,25 @@ namespace Assgn01_Calculator
 
                 else if (chars[i] == '+' || chars[i] == '-' || chars[i] == '*' || chars[i] == '/')
                 {
-                    /* if operators stack is empty, push current op to stack
-                     * if the next op encountered has more priority(precedence) than the last pushed op,
-                     * apply that operation on the last two numbers on the numbers stack */
-                    while(operators.Count > 0 && HasPrecedence(chars[i], operators.Peek()))
+                    /* If operators stack is empty, push current op to stack
+                     * If the next op encountered has more priority(precedence) than the last pushed op, push that op to top of operators stack 
+                     * If the next op has less priority, then perform ApplyOperator on the last pushed op, which should have more precedence than the current op*/
+                    while (operators.Count > 0 && HasPrecedence(chars[i], operators.Peek()))
                     {
                         numbers.Push(ApplyOperator(operators.Pop(), numbers.Pop(), numbers.Pop())); //apply current operator to the last 2 numbers on top of Stack
                     }
                     operators.Push(chars[i]);
                 }
             }
+            /* At this point, the entire input expression has been parsed and all the chars are in stacks. Now while there are ops in the operators stack,
+             * perform ApplyOperator() */
             while(operators.Count > 0)
             {
                 numbers.Push(ApplyOperator(operators.Pop(),numbers.Pop(), numbers.Pop()));
             }
             return numbers.Pop();
         }
-        /* From Ref 1, method to compare if current op has precedence over op on top of stack */
+        /* From Ref 1: method to compare if current op has precedence over op on top of stack */
         static bool HasPrecedence(char op1, char op2) 
         {
             if(op2 == '(' || op2 == ')')
@@ -96,7 +113,7 @@ namespace Assgn01_Calculator
             }
             else return true;
         }
-        /* From Ref 1, method to apply an operation to number b, the last number pushed to the numbers stack
+        /* From Ref 1: method to apply an operation to number b, the last number pushed to the numbers stack
          * and number a, the 2nd to the last number pushed to the numbers stack. 
          * Each return from ApplyOperator will be pushed to the numbers stack as seen in the Calculate() method, which
          * essentially mimics an expression tree as shown in the instructions document. Solving 1 operation and 2 numbers 
