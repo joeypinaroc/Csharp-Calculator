943e0caaafc0f7ba1af9a5af357c9b0fbda90e4b
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-10-03 19:49:33 -0600

    Added a way to account for decimals while building the number strings

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ using System.Threading.Tasks;
  * Reference 1 used Stacks instead of Lists, to conveniently pop() the elements 
  * on top of a char list while traversing the Stack and performing the math operations.
  * 
+ * Reference 2 below is from stackoverflow showing a ConvertToDouble method which I 
+ * used to convert decimal numbers to double
+ *      2. https://stackoverflow.com/questions/11399439/converting-string-to-double-in-c-sharp
  */
 namespace Assgn01_Calculator
 {
@@ -29,37 +32,59 @@ namespace Assgn01_Calculator
             {
                 if (chars[i] == ' ') continue; //if char is whitespace, skip
 
-                if (char.IsDigit(chars[i])) //check if current char is a digit
+                if (char.IsDigit(chars[i]) || chars[i] == '.') //check if current char is a digit OR a decimal point
                 {
-                    /* Reference 1 used a StringBuilder to create multi-digit numbers */
+                    /* Reference 1 used a StringBuilder for multi-digit numbers */
                     StringBuilder sb = new StringBuilder();
-                    while(i < chars.Length && char.IsDigit(chars[i]))
+                    bool hasDecimal = false; //to track if a decimal point is already found
+
+                    while(i < chars.Length && (char.IsDigit(chars[i]) || (chars[i] == '.' && !hasDecimal)))
                     {
+                        if (chars[i] == '.')
+                        {
+                            hasDecimal = true; //set flag once decimal is encountered
+                        }
                         sb.Append(chars[i++]);
                     }
-                    numbers.Push(ConvertStringToDouble(sb.ToString())); //convert multi-digit string to number and add to numbers list
+                    numbers.Push(ConvertStringToDouble(sb.ToString())); //convert multi-digit string to number and add to numbers stack
                     i--; //since i is now on the next digit, decrease i to correct the offset
+                }
+
+                else if (chars[i] == '(') //if char is opening bracket, push to operators
+                {
+                    operators.Push(chars[i]);
+                }
+
+                else if (chars[i] == ')') //if char is closing bracket, solve entire bracket
+                {
+                    /* keep doing ApplyOperator method inside bracket until all operators are popped */
+                    while (operators.Peek() != '(') 
+                    {
+                        numbers.Push(ApplyOperator(operators.Pop(), numbers.Pop(), numbers.Pop()));
+                    }
+                    operators.Pop(); //pop opener bracket after solving bracket
                 }
 
                 else if (chars[i] == '+' || chars[i] == '-' || chars[i] == '*' || chars[i] == '/')
                 {
-                    /* apply operator on top of operators to top two elements in numbers */
-                    while(operators.Count > 0 && hasPrecedence(chars[i], operators.Peek()))
+                    /* if operators stack is empty, push current op to stack
+                     * if the next op encountered has more priority(precedence) than the last pushed op,
+                     * apply that operation on the last two numbers on the numbers stack */
+                    while(operators.Count > 0 && HasPrecedence(chars[i], operators.Peek()))
                     {
                         numbers.Push(ApplyOperator(operators.Pop(), numbers.Pop(), numbers.Pop())); //apply current operator to the last 2 numbers on top of Stack
                     }
                     operators.Push(chars[i]);
                 }
             }
-            PrintNumList(numbers);
-            //PrintCharList(operators);
             while(operators.Count > 0)
             {
                 numbers.Push(ApplyOperator(operators.Pop(),numbers.Pop(), numbers.Pop()));
             }
             return numbers.Pop();
         }
-        static bool hasPrecedence(char op1, char op2)
+        /* From Ref 1, method to compare if current op has precedence over op on top of stack */
+        static bool HasPrecedence(char op1, char op2) 
         {
             if(op2 == '(' || op2 == ')')
             {
@@ -71,6 +96,11 @@ namespace Assgn01_Calculator
             }
             else return true;
         }
+        /* From Ref 1, method to apply an operation to number b, the last number pushed to the numbers stack
+         * and number a, the 2nd to the last number pushed to the numbers stack. 
+         * Each return from ApplyOperator will be pushed to the numbers stack as seen in the Calculate() method, which
+         * essentially mimics an expression tree as shown in the instructions document. Solving 1 operation and 2 numbers 
+         * at a time.*/
         static double ApplyOperator(char op, double b, double a)
         {
             switch (op)
@@ -90,20 +120,8 @@ namespace Assgn01_Calculator
             }
             return 0;
         }
-        static void PrintNumList(Stack<double> list)
-        {
-            foreach(var item in list)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        static void PrintCharList(Stack<char> list)
-        {
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-        }
+        /* Made method to convert input string, which can be decimal numbers, to double
+         * from Reference 2*/
         static double ConvertStringToDouble(string str)
         {
             if (str == null) return 0;
